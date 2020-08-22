fc81b73536c637a8a4d901613f09d3d6da03fb23
Gabriel Lima <51869362+GabrielLima1995@users.noreply.github.com>
2020-08-22 14:28:38 -0300

    Modificado funcao create features e RFE  22/08/2020 14:28

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -17,21 +17,6 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
 
-class Create_Features(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        return None
-    
-    def fit(self, X, y=None):
-        return self
-    
-    def transform(self, X,y):
-        
-        nota_humanas = np.median(X[:,[6,7,9]],axis = 1).reshape(-1,1)
-        nota_global =  np.median(X[:,[6,7,8,9]],axis=1).reshape(-1,1)
-        reprovacao =   np.median(X[:,[2,3,4,5]],axis =1).reshape(-1,1)
-        new_x = np.concatenate((X,nota_humanas,nota_global,reprovacao),axis=1)
-
-        return new_x,y
 
 class RFE_obj(BaseEstimator, TransformerMixin):
     
@@ -41,10 +26,9 @@ class RFE_obj(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
     
-    def transform(self, X,y):
+    def transform(self, X, y):
         
         r = RFE(self.model, step=1).fit(X,y)
-        cols = r.get_support(indices=True)
-        X_new= X[:,cols]       
-        
-        return X_new,y
+        cols = r.get_support(indices=True)      
+        X_new= X.iloc[:,cols] 
+        return X_new
